45ff5cefbc0dade5d5d71df32d1ac1d09aae077f
LHAWLEY <hawleyleeanne@gmail.com>
2015-12-07 05:50:10 -1000

    Fix comments in count between

--- week-4/count-between/my_solution.rb
+++ week-4/count-between/my_solution.rb
@@ -1,14 +1,18 @@
-# Longest String
+# Count Between
 
 # I worked on this challenge [by myself, with: ].
 
-# longest_string is a method that takes an array of strings as its input
-# and returns the longest string
+# count_between is a method with three arguments:
+#   1. An array of integers
+#   2. An integer lower bound
+#   3. An integer upper bound
 #
-# +list_of_words+ is an array of strings
-# longest_string(list_of_words) should return the longest string in +list_of_words+
+# It returns the number of integers in the array between the lower and upper bounds,
+# including (potentially) those bounds.
 #
-# If +list_of_words+ is empty the method should return nil
+# If +array+ is empty the method should return 0
+
+# Your Solution Below
 
 
 # Your Solution Below
