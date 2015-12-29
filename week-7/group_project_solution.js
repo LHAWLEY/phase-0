55fe83a2166b6cde9646d719cd63328e8ed4dde0
LHAWLEY <hawleyleeanne@gmail.com>
2015-12-29 07:12:17 -0500

    Add 7.8 group project pseuocode

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,30 @@
+// Pseudocode
+
+// Create a variable that takes an array of number elements (it can be contain any number of elements).
+// Create three separate operations that can all be run on your list of numbers.
+
+// Operation 1 should take all of the elements in your numbers variable and add them up to get the sum.
+
+// Operation 2 should find the average (mean) of the numbers variable.  To do so:
+  // First calculate the sum of all of the numbers.
+  // Then divide the sum by the number of elements in the variable.
+
+
+// Operation 3 (has two senarios depending on the number of elements in the numbers variable) you should find the middle (median) value of the elements in the numbers variable.  To do so:
+  // If there is an odd number of elements in the numbers variable:
+  //First order the numbers either from lowest to highest or highest to lowest.
+  // Then find the middle value of the numbers variable which is the median.
+
+  // If there is an even number of elements in the numbers variable:
+  // First order the numbers either from lowest to highest or highest to lowest.
+  // Find find the two middle values.
+  // Add the two middle values together then divide them by two.
+  // That should give you the median
+
+
+
+
+
+
+
+
